411fbcac3dec18f389b6581d1a4ae52fb8768955
Palashpunde2003 <palashpunde2003@gmail.com>
2024-07-31 17:35:29 +0530

    Deleted Dataset of multiple images

--- live_usage.py
+++ live_usage.py
@@ -1,14 +1,14 @@
 import warnings
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import LabelEncoder
 
 # Suppress specific warnings
 warnings.filterwarnings("ignore", category=UserWarning,
                          message=".*SymbolDatabase.GetPrototype.*")
-
-import cv2
-import mediapipe as mp
-import numpy as np
-from tensorflow.keras.models import load_model
-from sklearn.preprocessing import LabelEncoder
 
 model = load_model('sign_language_model.h5')
 label_encoder = LabelEncoder()
@@ -21,6 +21,11 @@ hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2,
 mp_drawing = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture(0)
+
+word_buffer = []
+letter_buffer = []
+last_time = time.time()
+buffer_time = 5.0
 
 while cap.isOpened():
     success, image = cap.read()
@@ -39,7 +44,24 @@ while cap.isOpened():
             class_name = label_encoder.inverse_transform([class_id])[0]
 
             mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-            cv2.putText(image, class_name, (100, 300), cv2.FONT_HERSHEY_SIMPLEX, 10, (255, 0, 0), 10, cv2.LINE_AA)
+            cv2.putText(image, class_name, (100, 300), cv2.FONT_HERSHEY_SIMPLEX,
+                         10, (255, 0, 0), 10, cv2.LINE_AA)
+
+            if class_name:
+                current_time = time.time()
+                if (current_time - last_time > buffer_time):
+                    if class_name not in letter_buffer:
+                        letter_buffer.append(class_name)
+                        last_time = current_time 
+
+                if len(letter_buffer) > 0:
+                    word_buffer.append(''.join(letter_buffer))
+                    letter_buffer.clear()
+
+                if len(word_buffer) > 0:
+                    word_display = ' '.join(word_buffer)
+                    cv2.putText(image, word_display, (50, 450), cv2.FONT_HERSHEY_SIMPLEX,
+                                1, (0, 255, 0), 2, cv2.LINE_AA)
 
     cv2.imshow('Sign Language Detection', image)
 
